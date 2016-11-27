c87a2b49c37e2601005a81caa9e8fe63ca2cb698
Minh Nguyen <minhnguyen_96@hotmail.com>
2016-11-27 13:16:03 +1000

    change wacc to percent

--- ReactFinApp/src/index.js
+++ ReactFinApp/src/index.js
@@ -119,8 +119,8 @@ class WeightedAverageCostOfCapital extends Component
 		var _debt = parseFloat(this.state.debt);
 		var _equity = parseFloat(this.state.equity);
 		var _value = _equity + _debt;
-		var _rd = parseFloat(this.state.rd);
-		var _re = parseFloat(this.state.re);
+		var _rd = parseFloat(this.state.rd)/100;
+		var _re = parseFloat(this.state.re)/100;
 		var _wacc = (_re*(_equity/_value)) + (_rd*(_debt/_value));
 		return Math.round(_wacc * 10000)/100;
 	}
@@ -152,8 +152,8 @@ class WeightedAverageCostOfCapital extends Component
 			  <h3> Weighted Average Cost of Capital </h3>
 			  <p> Equity </p><input type="text" value={this.state.value} onChange={this.handleEquityChange} />
 			  <p> Debt </p> <input type="text" value={this.state.value} onChange={this.handleDebtChange} />
-			  <p> Return on Equity </p> <input type="text" value={this.state.value} onChange={this.handleReChange} />
-			  <p> Return on Debt </p> <input type="text" value={this.state.value} onChange={this.handleRdChange} />
+			  <p> Return on Equity (%) </p> <input type="text" value={this.state.value} onChange={this.handleReChange} />
+			  <p> Return on Debt (%) </p> <input type="text" value={this.state.value} onChange={this.handleRdChange} />
 			  <p> Weighted Average Cost of Capital: {this.wacc()}%</p>
 			</div>
 		);
