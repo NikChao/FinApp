fe59b0c9a95a4fecf6f7a580b426da4cd1521c2a
Minh Nguyen <minhnguyen_96@hotmail.com>
2016-12-03 23:38:23 +1000

    Working on creating a table of options that changes based on options that user inputs into an array

--- ReactFinApp/src/Formulas/FinancialOptions.js
+++ ReactFinApp/src/Formulas/FinancialOptions.js
@@ -11,57 +11,80 @@ export default class FinancialOptions extends Component
         // state
         this.state =
         {
-        }
+            // arrayOfOptions: [],      // Elements should be tuple of option name and option price
+            // name: "",        // to retrieve text in name input
+            // price: "",       // to retrieve text in price input
+
+            // Messing arround below
+            currentArray: ['One', 'Two', 'Three', 'Four'],      // array of names
+            name: 'Five',  // conversion ratio
+            x: ""     // face value
+        };
 
         // method binding
+        this.makeTableFromOptionArray = this.makeTableFromOptionArray.bind(this);
+        this.handleAddOption = this.handleAddOption.bind(this);
+
+    }
+
+    handleAddOption(event)
+    {
+        const newArray = this.state.currentArray;
+        newArray.add(this.state.name);
+        this.setState({name: event.target.value});
+        this.setState({currentArray: newArray});
+    }
+
+    makeTableFromOptionArray (name, x)
+    {
+    // Should add a new row for each element in array - for option
+    // Should add column for each element in array - for range 0 < X < 50... etc....
+    // Will need a function that mutates the table based on number of elements in array
+    // table should be scrollable sideways
+    return (
+        <div>
+            <table>
+                <thead>
+                    <th> Option </th>
+                    <th> P1 </th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th> {this.state.currentArray[2]} </th>
+                        <th> {this.state.x} </th>
+                    </tr>
+                    <tr>
+                        <th> {this.state.currentArray[3]} </th>
+                        <th> {this.state.x} </th>
+                    </tr>
+                </tbody>
+            </table>
+        </div>);
+
     }
 
     render()
     {
         return (
             <div>
-                <table>
-                    <p><h4> Basic Instruments </h4></p>
-                    <tr>
-                        <td> Instruments</td>
-                        <td>  Long </td>
-                        <td> short </td>
-                    </tr>
-                    <tr>
-                        <td> Bonds </td>
-                        <td>  </td>
-                        <td>  </td>
-                    </tr>
-                    <tr>
-                        <td>  Stocks</td>
-                        <td>  </td>
-                        <td>  </td>
-                    </tr>
-                    <tr>
-                        <td>  Forward/Future Contracts</td>
-                        <td>  </td>
-                        <td>  </td>
-                    </tr>
-                </table>
-                <table>
-                    <p><h4> Options </h4></p>
-                    <tr>
-                        <td> Options</td>
-                        <td>  Long </td>
-                        <td> short </td>
-                    </tr>
-                    <tr>
-                        <td> Call </td>
-                        <td>  </td>
-                        <td>  </td>
-                    </tr>
-                    <tr>
-                        <td>  Put</td>
-                        <td>  </td>
-                        <td>  </td>
-                    </tr>
-                </table>
+                <div>
+                    <p> <strong> Option Form </strong> </p>
+                    <Input label="Option"/>
+                    <Input label="Exercise Price"/>
+                    <Button  waves='light' onClick={this.handleAddOption}>Add Option</Button>
+                </div>
+                <p> Pay-Off Table <br/>  {this.makeTableFromOptionArray()} </p>
             </div>
-        );
+        )
     }
 }
+
+/*
+            <div>
+                <Input label="Option" />
+                <Input label="Exercise Price" />
+                <Button label="Add Option" />
+                {this.makeTableFromOptionArray()}
+
+            </div>
+ */
